b21d68ea7b6345d22ea8e8d31d4a362890a5ae4a
mohtareq1999mm-lab <mohtareq1999mm@gmail.com>
2026-09-13 09:12:05 +0300

    update export storage to use 'imports' disk and improve filename handling

--- check_log_images.php
+++
@@ -1,8 +0,0 @@
-<?php
-$log=file_get_contents('storage/logs/laravel.log');
-$pos=strrpos($log,'ImportProductImagesJob');
-if($pos!==false) echo substr($log,$pos-500,2000)."\n"; else echo "no ImportProductImagesJob in log\n";
-$pos=strrpos($log,'dispatchImage');
-if($pos!==false) echo substr($log,$pos-500,2000)."\n"; else echo "no dispatchImage\n";
-$pos=strrpos($log,'product.import');
-echo substr($log,strrpos($log,'product.import')-300,800)."\n";
